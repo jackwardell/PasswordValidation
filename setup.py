46a877b6e081fcbe1e02c427df55c15acb690742
jackwardell <jackwardell@me.com>
2020-08-06 10:37:02 +0100

    adding more to setup

--- setup.py
+++ setup.py
@@ -9,7 +9,14 @@ setup(
     version="0.0.1",
     author="Jack Wardell",
     long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jackwardell/PasswordValidation",
     packages=find_packages(),
     include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     python_requires='>=3.6',
 )
